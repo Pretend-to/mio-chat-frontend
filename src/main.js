86696ad3f1d8bae003cabca53ff2ea5b7c1c8cd8
彭志勇 <2457305628@qq.com>
2024-04-19 18:44:03 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@
  * @lastEditTime 2024-04-15 16:44:27
  */
 
-import './assets/main.sass'
-import '//at.alicdn.com/t/c/font_4459318_cmd4krl96d.js' // 项目图标库
+import './assets/global.sass'
+import '//at.alicdn.com/t/c/font_4459318_ioeido0jcz.js' // 项目图标库
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
