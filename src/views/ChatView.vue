4a78d6a4283503953f66046b92c8943afb78da77
Mio-FCIP <1099834705@qq.com>
2024-04-22 22:50:32 +0800

    init: init fullScreen

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -344,7 +344,7 @@ export default {
                                 fill="#333333" p-id="4522"></path>
                         </svg>
                     </div>
-                    <div class="button" @click="waiting()" id="max">
+                    <div class="button" @click="client.fullScreen = !client.fullScreen" id="max">
                         <svg t="1696841744276" class="icon" viewBox="0 0 1024 1024" version="1.1"
                             xmlns="http://www.w3.org/2000/svg" p-id="4306" width="16" height="16">
                             <path
